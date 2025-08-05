d7d52673401495b20476f966f0117bf665b13da8
Angel Nuw <Angel.Nuw@mssa.newhorizons.com>
2025-08-05 16:25:45 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment5._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string: ");
+            string input = Console.ReadLine().Trim();
+            int length = 0;
+
+            if (input.Length > 0)
+            {
+                int lastIndex = input.LastIndexOf(' ');
+                length = input.Length - lastIndex - 1;
+            }
+            Console.WriteLine("Length of the last word: " + length);
+        }
+    }
+}
